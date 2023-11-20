a6b42282b4f270ab7044e3858a074938f574732c
Enab13d <extendeddirection@gmail.com>
2023-11-20 10:58:19 +0200

    Fixed test for login controller

--- controllers/auth.test.js
+++ controllers/auth.test.js
@@ -1,31 +1,35 @@
-
 const request = require("supertest");
 const app = require("../app");
-
-const payload = { email: "billyharington@mail.com", password: "quiteComplicated" };
+const mongoose = require("mongoose");
+const http = require("http");
+const uriDB = process.env.DB_HOST;
+mongoose.connect(uriDB);
+const { PORT = 33333 } = process.env;
+const payload = {
+  email: "billyharington@mail.com",
+  password: "quiteComplicated",
+};
+const server = http.createServer(app);
 describe("login controller test", () => {
-let server;
-    beforeAll(()=> server = app.listen(3000));
-    afterAll(()=> server.close());
+  beforeAll(() => {
+    server.listen(PORT);
+  });
+  afterAll(() => {
+    mongoose.connection.close();
+    server.close();
+  });
   it("login controller returns response with status code 200", async () => {
-    const response = await request(app)
-      .post("/api/users/login")
-      .send(payload)
-    
+    const response = await request(app).post("/api/users/login").send(payload);
+
     expect(response.status).toBe(200);
   });
   it("response should return token", async () => {
-    const response = await request(app)
-      .post("/api/users/login")
-      .send(payload);
-    expect(response.token).toBe(!undefined);
+    const response = await request(app).post("/api/users/login").send(payload);
+    expect(response.body.token).not.toBe(undefined);
   });
-  it("response should contain an object with field email and subscription with data type of String", async () => {
-    const response = await request(app)
-      .post("/api/users/login")
-      .send(payload);
-    expect(typeof response.email).toBe("string");
-    expect(typeof response.subscription).toBe("string");
+  it("response should contain an object with fields 'email' and 'subscription' both with data type of String", async () => {
+    const response = await request(app).post("/api/users/login").send(payload);
+    expect(typeof response.body.user.email).toBe("string");
+    expect(typeof response.body.user.subscription).toBe("string");
   });
 });
-
